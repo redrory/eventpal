7bef7283477ee54d93eacbb6a87075042d305d75
Rory Walker <redrory@gmail.com>
2012-06-24 19:23:33 -0500

    Remove duplicate devise view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Eventpal::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {:confirmations => 'confirmations'}
+
+  devise_scope :user do
+    put "/confirm" => "confirmations#confirm"
+  end
+  
+  get "confirmations/show"
+
+  get "confirmations/confirm"
 
   post "events/create_food"
 
